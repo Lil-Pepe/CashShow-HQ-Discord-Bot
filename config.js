11d7dc4b013f52a181312c0b8c514ee4dbedf0b9
Lil Pepe <42453314+Lil-Pepe@users.noreply.github.com>
2018-09-08 15:34:38 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,6 @@ var config = {
   width: 1920, //Width of your screen
   google: 'https://www.google.com/search?q=',
   bing: 'https://www.bing.com/search?q=',
-  d: [],
-  p: [0, 0, 0], //Defining the
   notWords: ['not', 'n\'est pas', 'kein', 'keine', 'keinen', 'n\'à', 'n\'a', 'nicht', 'none', 'jamais', 'never', 'nie', 'niemals', 'nowhere'], //Negative words
   ignoreList: ['des', 'les', 'his', 'and', 'her', 'der', 'die', 'das', 'als', 'the', 'une', 'ein'] // Words to ignore
 };
