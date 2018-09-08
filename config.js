88e3c18dfd55f933b0e1e37eaaef24af90b0b1b1
Lil Pepe <42453314+Lil-Pepe@users.noreply.github.com>
2018-09-08 15:27:10 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+var config = {
+  userId: 'YOUR_DISCORD_USER_ID', //Your Discord user ID
+  command: '-g', // Bot trigger command
+  token: 'DISCORD_BOT_TOKEN', //Discord bot token
+  width: 1920, //Width of your screen
+  google: 'https://www.google.com/search?q=',
+  bing: 'https://www.bing.com/search?q=',
+  d: [],
+  p: [0, 0, 0], //Defining the
+  notWords: ['not', 'n\'est pas', 'kein', 'keine', 'keinen', 'n\'à', 'n\'a', 'nicht', 'none', 'jamais', 'never', 'nie', 'niemals', 'nowhere'], //Negative words
+  ignoreList: ['des', 'les', 'his', 'and', 'her', 'der', 'die', 'das', 'als', 'the', 'une', 'ein'] // Words to ignore
+};
+
+module.exports = config;
